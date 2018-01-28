b8f1dd613c631d403d331696ca8c93ae6b9b56e7
Justin Calleja <calleja.justin@gmail.com>
2018-01-28 11:22:59 +0100

    Reformat using .prettierrc config

--- test/client/index.js
+++ test/client/index.js
@@ -1,17 +1,17 @@
-const superagent = require("superagent");
+const superagent = require('superagent');
 
 function getPizzas() {
-  return superagent.get("http://localhost:8061/api/v1/pizzas");
+    return superagent.get('http://localhost:8061/api/v1/pizzas');
 }
 
 function postPizza() {
-  return superagent
-    .post("http://localhost:8061/api/v1/pizzas")
-    .send({ name: "margherita" })
-    .set('X-API-Key', 'foobar');
+    return superagent
+        .post('http://localhost:8061/api/v1/pizzas')
+        .send({ name: 'margherita' })
+        .set('X-API-Key', 'foobar');
 }
 
 module.exports = {
-  getPizzas,
-  postPizza
+    getPizzas,
+    postPizza,
 };
